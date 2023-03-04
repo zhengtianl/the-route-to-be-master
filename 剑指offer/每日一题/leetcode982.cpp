a6b978ac54d7232b75b038aed0cecfc3aeca921f
zhengtianl <458078290@qq.com>
2023-03-04 18:14:05 +1100

    change

---
+++ 剑指offer/每日一题/leetcode982.cpp
@@ -0,0 +1,33 @@
+#include "unordered_map"
+#include "iostream"
+using namespace std;
+
+class Solution 
+{
+public:
+    int countTriplets(vector<int>& nums) 
+    {
+        int ans = 0;
+        unordered_map<int, int> cnt(1<<16);
+        for(int x:nums)
+        {
+            for(int y: nums)
+            {
+                ++cnt[x & y];
+            }
+        }
+
+        for(int x: nums)
+        {
+            for(int j = 0; j < (1 << 16); j++)
+            {
+                if((x & j) == 0)
+                {
+                    ans += cnt[j];
+                }
+            }
+        }
+        return ans;
+    }
+};
+
