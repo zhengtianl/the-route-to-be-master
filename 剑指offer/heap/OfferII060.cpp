b784aa4b0b5aef0c8691060666d440ab0d3f5c00
zhengtianl <458078290@qq.com>
2023-03-06 13:25:57 +1100

    fix

--- 剑指offer/heap/OfferII060.cpp
+++ 剑指offer/heap/OfferII060.cpp
@@ -1,34 +1,40 @@
-#include "queue"
-#include "vector"
-using namespace std;
-
+// 时间复杂度：O(nlogk)
+// 空间复杂度：O(n)
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        // 最大堆
-        auto cmp = [&](const pair<int, int>& lhs, const pair<int, int>& rhs) {
-            return lhs.first + lhs.second < rhs.first + rhs.second;
-        };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> heap(cmp);
-        for (int i = 0; i < nums1.size() && i < k; ++i) {
-            for (int j = 0; j < nums2.size() && j < k; ++j) {
-                if (heap.size() < k) {
-                    heap.push({nums1[i], nums2[j]});
-                }
-                else if (nums1[i] + nums2[j] < heap.top().first + heap.top().second)  {
-                    heap.pop();
-                    heap.push({nums1[i], nums2[j]});
-                }
+    // 小顶堆
+    class mycomparison {
+    public:
+        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+            return lhs.second > rhs.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // 要统计元素出现频率
+        unordered_map<int, int> map; // map<nums[i],对应出现的次数>
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]]++;
+        }
+
+        // 对频率排序
+        // 定义一个小顶堆，大小为k
+        priority_queue<pair<int, int>, vector<pair<int, int>>, mycomparison> pri_que;
+
+        // 用固定大小为k的小顶堆，扫面所有频率的数值
+        for (unordered_map<int, int>::iterator it = map.begin(); it != map.end(); it++) {
+            pri_que.push(*it);
+            if (pri_que.size() > k) { // 如果堆的大小大于了K，则队列弹出，保证堆的大小一直为k
+                pri_que.pop();
             }
         }
-        int size = heap.size();
-        vector<vector<int>> ret(size, vector<int>(2, 0));
-        for (int i = 0; i < size; ++i) {
-            ret[size - 1 - i][0] = heap.top().first;
-            ret[size - 1 - i][1] = heap.top().second;
-            heap.pop();
+
+        // 找出前K个高频元素，因为小顶堆先弹出的是最小的，所以倒序来输出到数组
+        vector<int> result(k);
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pri_que.top().first;
+            pri_que.pop();
         }
-        return ret;
+        return result;
+
     }
 };
-
