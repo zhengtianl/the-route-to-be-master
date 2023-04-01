9ea613b0b5a074a7a0cfbf1f098082146d273930
zhengtianl <458078290@qq.com>
2023-04-01 14:19:16 +1100

    change daily

---
+++ 剑指offer/每日一题/leetcode831.cpp
@@ -0,0 +1,23 @@
+//
+// Created by StarkLu on 2023/4/1.
+//
+#include "string"
+#include "vector"
+#include "algorithm"
+using namespace std;
+class Solution {
+public:
+    vector<string> country = {"", "+*-", "+**-", "+***-"};
+
+    string maskPII(string s) {
+        string res;
+        int at = s.find("@");
+        if (at != string::npos) {
+            transform(s.begin(), s.end(), s.begin(), ::tolower);
+            return s.substr(0, 1) + "*****" + s.substr(at - 1);
+        }
+        s = regex_replace(s, regex("[^0-9]"), "");
+        return country[s.size() - 10] + "***-***-" + s.substr(s.size() - 4);
+    }
+};
+
