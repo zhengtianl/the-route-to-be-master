706f803c2bbc7a76bf98e418be0228cf296f3a6c
zhengtianl <458078290@qq.com>
2023-05-19 17:02:56 +1000

    stack

---
+++ acm/daima232.cpp
@@ -0,0 +1,43 @@
+//
+// Created by StarkLu on 2023/5/19.
+//
+#include "iostream"
+#include "algorithm"
+#include "stack"
+using namespace std;
+class MyQueue{
+public:
+    stack<int> stIn;
+    stack<int> stOut;
+    MyQueue(){};
+    void push(int x)
+    {
+        stIn.push(x);
+    }
+    int pop()
+    {
+        if(stOut.empty())
+        {
+            while(!stIn.empty())
+            {
+                stOut.push(stIn.top());
+                stIn.pop();
+            }
+        }
+        int result = stOut.top();
+        stOut.pop();
+        return result;
+    }
+
+    int peek()
+    {
+        int res = this -> pop();
+        stOut.push(res);
+        return res;
+
+    }
+    bool empty()
+    {
+        return stIn.empty() && stOut.empty();
+    }
+};
