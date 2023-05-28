7d62fb11e174a3d99a694e34c8022244a1f4cc38
zhengtianl <458078290@qq.com>
2023-05-28 17:07:10 +1000

    change

---
+++ 代码随想录训练营/day4(142).py
@@ -0,0 +1,38 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head):
+        visited = set()
+        while head is not None:
+            if head in visited:
+                return head
+            visited.add(head)
+            head = head.next
+        return None
+
+# Test case
+# Create a cycle: 1 -> 2 -> 3 -> 4 -> 2 (cycle)
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2  # cycle
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the detectCycle method
+result = solution.detectCycle(node1)
+
+# Check if a cycle was detected and print the starting node of the cycle
+if result is not None:
+    print("Cycle detected! Start of the cycle:", result.val)
+else:
+    print("No cycle detected.")
