cc396bd81f64cb07b25d90847ded123f59181a1d
zhengtianl <458078290@qq.com>
2023-06-09 15:33:32 +1000

    1

--- 代码随想录训练营/test.cpp
+++ 代码随想录训练营/test.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
-using namespace std;
-int main(){
-    const int num=0;
-    const int * const ptr=&num; //error! const int* -> int*
-    cout<<*ptr<<endl;
+#include "DataStruct.h"
+class Solution{
+    int max = 0;
+    public int maxDepth(TreeNode root){
+        ans(root, 0);
+        return max
+    }
 }
