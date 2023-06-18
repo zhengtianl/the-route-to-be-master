37658465c39225e58b677d0b4c7d570d81922fed
zhengtianl <458078290@qq.com>
2023-06-18 14:49:37 +1000

    change

---
+++ 代码随想录训练营/day24(77).cpp
@@ -0,0 +1,27 @@
+//
+// Created by StarkLu on 2023/6/18.
+//
+#include "vector"
+using namespace std;
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++) { // 优化的地方
+            path.push_back(i); // 处理节点
+            backtracking(n, k, i + 1);
+            path.pop_back(); // 回溯，撤销处理的节点
+        }
+    }
+public:
+
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return result;
+    }
+};
