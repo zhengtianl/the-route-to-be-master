a1644e150d984302297a302ac943805f647df715
zhengtianl <458078290@qq.com>
2023-06-27 12:28:11 +1000

    bubble sort

---
+++ 代码随想录训练营/bubblesort.py
@@ -0,0 +1,7 @@
+def bubblesort(nums):
+    n = len(nums)
+    for i in range(1,n):
+        for j in range(0,n - i):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j + 1], nums[j]
+    return nums
