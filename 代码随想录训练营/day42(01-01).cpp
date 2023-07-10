c04413c99b4576f586b8044004888a2968718c8b
zhengtianl <458078290@qq.com>
2023-07-10 17:24:28 +0800

    change

---
+++ 代码随想录训练营/day42(01-01).cpp
@@ -0,0 +1,22 @@
+#include "vector"
+#include "algorithm"
+#include "iostream"
+using namespace std;
+void test_1_wei_bag_problem() {
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+
+    // 初始化
+    vector<int> dp(bagWeight + 1, 0);
+    for(int i = 0; i < weight.size(); i++) { // 遍历物品
+        for(int j = bagWeight; j >= weight[i]; j--) { // 遍历背包容量
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+    }
+    cout << dp[bagWeight] << endl;
+}
+
+int main() {
+    test_1_wei_bag_problem();
+}
