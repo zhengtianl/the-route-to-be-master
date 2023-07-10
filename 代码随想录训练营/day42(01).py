ef9d43517f25074fc216704d4e54f3cb73d110b8
zhengtianl <458078290@qq.com>
2023-07-10 17:21:05 +0800

    change

---
+++ 代码随想录训练营/day42(01).py
@@ -0,0 +1,23 @@
+def test_2_wei_bag_problem1():
+    weight = [1, 3, 4]
+    value = [15, 20, 30]
+    bagweight = 4
+
+    # 二维数组
+    dp = [[0] * (bagweight + 1) for _ in range(len(weight))]
+
+    # 初始化
+    for j in range(weight[0], bagweight + 1):
+        dp[0][j] = value[0]
+
+    # weight数组的大小就是物品个数
+    for i in range(1, len(weight)):  # 遍历物品
+        for j in range(bagweight + 1):  # 遍历背包容量
+            if j < weight[i]:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i])
+
+    print(dp[len(weight) - 1][bagweight])
+
+test_2_wei_bag_problem1()
