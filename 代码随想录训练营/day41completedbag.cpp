ce133d96960fb0805f5a754a02198ad8af081347
zhengtianl <458078290@qq.com>
2023-07-11 18:21:03 +0800

    change

---
+++ 代码随想录训练营/day41completedbag.cpp
@@ -0,0 +1,19 @@
+# include "vector"
+# include "iostream"
+using namespace std;
+
+void test_CompletePack() {
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+    vector<int> dp(bagWeight + 1, 0);
+    for(int i = 0; i < weight.size(); i++) { // 遍历物品
+        for(int j = weight[i]; j <= bagWeight; j++) { // 遍历背包容量
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+    }
+    cout << dp[bagWeight] << endl;
+}
+int main() {
+    test_CompletePack();
+}
