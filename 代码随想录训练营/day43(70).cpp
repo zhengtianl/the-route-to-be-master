5446e13e40c31954a388d07903504ed19c9b03ad
zhengtianl <458078290@qq.com>
2023-07-12 22:21:30 +0800

    change

---
+++ 代码随想录训练营/day43(70).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[n+2];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        return dp[n];
+    }
+};
