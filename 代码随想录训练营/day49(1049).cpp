5a759cc1ea1054b8bcf3c24a137bd11aec6b7e39
zhengtianl <458078290@qq.com>
2023-07-16 14:16:14 +0800

    change

---
+++ 代码随想录训练营/day49(1049).cpp
@@ -0,0 +1,18 @@
+#include "vector"
+#include "algorithm"
+using namespace std;
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        vector<int> dp(15001, 0);
+        int sum = 0;
+        for (int i = 0; i < stones.size(); i++) sum += stones[i];
+        int target = sum / 2;
+        for (int i = 0; i < stones.size(); i++) { // 遍历物品
+            for (int j = target; j >= stones[i]; j--) { // 遍历背包
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        return sum - dp[target] - dp[target];
+    }
+};
