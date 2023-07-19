3ff0ee12824ff5aff0b33e3e2c8c81163dd4b4a3
zhengtianl <458078290@qq.com>
2023-07-19 12:30:31 +0800

    change

---
+++ 代码随想录训练营/day57(674).py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        dp = [[False] * len(s) for _ in range(len(s))]
+        result = 0
+        for i in range(len(s)-1, -1, -1): #注意遍历顺序
+            for j in range(i, len(s)):
+                if s[i] == s[j] and (j - i <= 1 or dp[i+1][j-1]): 
+                    result += 1
+                    dp[i][j] = True
+        return result
