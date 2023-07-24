00699f7cbab148c95f8e7f46ea59214b386fa3b9
zhengtianl <stark@U-R7TX7QJ2-2255.local>
2023-07-24 09:25:02 +0800

    change

---
+++ daily/numJewelsInStones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int ans = 0;
+        int jewelsLength = jewels.length(), stoneLength = stones.length();
+        for(int i = 0; i < stoneLength; i++){
+            char stone = stones.charAt(i);
+            for (int j = 0; j < jewelsLength; j++){
+                char jewel = jewels.charAt(j);
+                if(stone == jewel){
+                    ans ++;
+                }
+            }
+        }
+        return ans;
+    }
+}
