c54677ace3405414e69dbb9fdf3974c2a7ad4daa
zhengtianl <458078290@qq.com>
2023-12-04 10:21:21 +0800

    change

--- 代码随想录训练营/HOT100/11.py
+++ 代码随想录训练营/HOT100/11.py
@@ -1,17 +1,11 @@
-#给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
-def rain(nums):
-    left =  0
-    right = len(nums) - 1
-    left_max = 0
-    right_max = 0
-    ans = 0
-    while left < right:
-        left_max = max(left_max, nums[left])
-        right_max = max(right_max, nums[right])
-        if nums[left] < nums[right]:
-            ans += left_max - nums[left]
-            left += 1
+
+def maxArea(height):
+    i, j, res = 0, len(height) - 1, 0
+    while i < j:
+        if height[i] < height[j]:
+            res = max(res, height[i] * abs(j - i))
+            i += 1
         else:
-            ans += right_max - nums[right]
-            right -= 1
-    return ans
+            res = max(res, height[j] * abs(i -  j))
+            j -= 1
+    return res
