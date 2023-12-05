e87fcec65f65c8cba08a0d0d26f58c764b1e7503
zhengtianl <458078290@qq.com>
2023-12-05 15:12:04 +0800

    change

--- 代码随想录训练营/HOT100/15.py
+++ 代码随想录训练营/HOT100/15.py
@@ -1,34 +1,29 @@
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3:
+        n=len(nums)
+        res=[]
+        if(not nums or n<3):
             return []
-
         nums.sort()
-        ans = []
-        for k in range(len(nums) - 2):
-            if nums[k] > 0:
-                return ans
-
-            if k > 0 and nums[k] == nums[k-1]: continue
-            l = k + 1
-            r = len(nums) - 1
-            while l < r:
-                s = nums[k] + nums[l] + nums[r]
-                if s == 0:
-                    ans.append([nums[k], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1
-                elif s < 0:
-                    l += 1
+        res=[]
+        for i in range(n):
+            if(nums[i]>0):
+                return res
+            if(i>0 and nums[i]==nums[i-1]):
+                continue
+            L=i+1
+            R=n-1
+            while(L<R):
+                if(nums[i]+nums[L]+nums[R]==0):
+                    res.append([nums[i],nums[L],nums[R]])
+                    while(L<R and nums[L]==nums[L+1]):
+                        L=L+1
+                    while(L<R and nums[R]==nums[R-1]):
+                        R=R-1
+                    L=L+1
+                    R=R-1
+                elif(nums[i]+nums[L]+nums[R]>0):
+                    R=R-1
                 else:
-                    r -= 1
-
-        return ans
-            
-            
-        
+                    L=L+1
+        return res
