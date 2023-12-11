86bcec49a6a2eb1343e24d2c79df0f6f1c495b80
zhengtianl <458078290@qq.com>
2023-12-11 11:43:47 +0800

    change

--- 代码随想录训练营/HOT100/206.py
+++ 代码随想录训练营/HOT100/206.py
@@ -1,10 +1,12 @@
-def _init_(self, val = 0, next = None):
-    self.val = val
-    self.next = next
+class ListNode:
+    def _init_(self, val = 0, next = None):
+        self.val = val
+        self.next = next
 
 
-def reverseList(self, head):
-    cur, pre = head, None
-    while cur:
-        cur.next, pre, cur = pre, cur, cur.next
-    return pre
+class Solution:
+    def reverseList(self, head):
+        cur, pre = head, None
+        while cur:
+            cur.next, pre, cur = pre, cur, cur.next
+        return pre
---
+++ 代码随想录训练营/HOT100/234.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        self.front_pointer = head
+
+        def recursively_check(current_node=head):
+            if current_node is not None:
+                if not recursively_check(current_node.next):
+                    return False
+                if self.front_pointer.val != current_node.val:
+                    return False
+                self.front_pointer = self.front_pointer.next
+            return True
+
+        return recursively_check()
